4afe35541d4f459309e1ecdd7f255e687aa3ee7d
rakabgs27 <rakabgs2000@gmail.com>
2023-05-28 21:19:00 +0700

    Setuo

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -4,6 +4,8 @@ function Example() {
   return (
     <div>
       <h1>Hello, React Raka Rafif Thirsya</h1>
+      <h1>Hello, React Raka Rafif Thirsya</h1>
+      <h1>Hello, React Raka Rafif Thirsya</h1>
     </div>
   );
 }
